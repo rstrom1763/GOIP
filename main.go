284b2986cbebb81d0d8e691deac0c82b9f576ed1
Ryan <48137622+rstrom1763@users.noreply.github.com>
2022-11-29 21:35:49 -0600

    Experimenting with ping

--- main.go
+++ main.go
@@ -9,4 +9,14 @@ import (
 func main() {
 	args := os.Args
 	fmt.Println(args[1])
+	pinger, err := ping.NewPinger("www.google.com")
+	if err != nil {
+		panic(err)
+	}
+	pinger.Count = 3
+	err = pinger.Run() // Blocks until finished.
+	if err != nil {
+		panic(err)
+	}
+	stats := pinger.Statistics() // get send/receive/duplicate/rtt stats
 }
