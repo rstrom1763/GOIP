bec6be2ddb865eada78c0b36de4dea930eba5c5e
Ryan <48137622+rstrom1763@users.noreply.github.com>
2022-12-03 23:19:42 -0600

    Update main.go

--- main.go
+++ main.go
@@ -1,64 +1,33 @@
 package main
 
 import (
-	"net"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
 )
 
 // This is just a placeholder for now
 func main() {
-	newConns := make(chan net.Conn, 128)
-	deadConns := make(chan net.Conn, 128)
-	publishes := make(chan []byte, 128)
-	conns := make(map[net.Conn]bool)
-	listener, err := net.Listen("tcp", ":8080")
+	args := os.Args
+	uri := args[1]
+
+	if len(args) < 2 || len(args) > 2 {
+		fmt.Printf("Usage: ./http-get <url>\n")
+		os.Exit(1)
+	}
+
+	//Validate whether url is valid or not
+	if _, err := url.ParseRequestURI(uri); err != nil {
+		fmt.Println("URL is not valid! ")
+		os.Exit(1)
+	}
+
+	response, err := http.Get(uri)
 	if err != nil {
-		panic(err)
+		fmt.Println("Request Failed!")
+		os.Exit(1)
+	} else {
+		fmt.Println(response)
 	}
-	go func() {
-		for {
-			conn, err := listener.Accept()
-			if err != nil {
-				panic(err)
-			}
-			newConns <- conn
-		}
-	}()
-	for {
-		select {
-		case conn := <-newConns:
-			conns[conn] = true
-			go func() {
-				buf := make([]byte, 1024)
-				for {
-					nbyte, err := conn.Read(buf)
-					if err != nil {
-						deadConns <- conn
-						break
-					} else {
-						fragment := make([]byte, nbyte)
-						copy(fragment, buf[:nbyte])
-						publishes <- fragment
-					}
-				}
-			}()
-		case deadConn := <-deadConns:
-			_ = deadConn.Close()
-			delete(conns, deadConn)
-		case publish := <-publishes:
-			for conn, _ := range conns {
-				go func(conn net.Conn) {
-					totalWritten := 0
-					for totalWritten < len(publish) {
-						writtenThisCall, err := conn.Write(publish[totalWritten:])
-						if err != nil {
-							deadConns <- conn
-							break
-						}
-						totalWritten += writtenThisCall
-					}
-				}(conn)
-			}
-		}
-	}
-	listener.Close()
 }
